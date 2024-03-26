075aad18cf5da6536393c087603eda17096bbbef
Dmitry Tavern <dmytrotraktirnyi@gmail.com>
2024-03-26 15:51:53 +0200

    feat(Project workshop): add rating to server app

--- Project workshop/UniversityServer/ViewModels/RatingViewModel.cs
+++ Project workshop/UniversityServer/ViewModels/RatingViewModel.cs
@@ -1,7 +1,61 @@
-﻿namespace UniversityServer.ViewModels
+﻿using System.Windows;
+using UniversityServer.Database;
+
+namespace UniversityServer.ViewModels
 {
+    public class RatingTeacherData(int number, string name, double hours, Teachers teacher, List<Raports> raports)
+    {
+        public int number { get; set; } = number;
+        public string name { get; } = name;
+        public double hours { get; } = hours;
+        public Teachers teacher { get; } = teacher;
+        public List<Raports> raports { get; } = raports;
+    }
+
     public class RatingViewModel : ViewModelBase
     {
-        public string Title { get; } = "Rating page";
+        private List<RatingTeacherData> teachersData { get; set; } = [];
+        public List<RatingTeacherData> TeachersData
+        {
+            get { return teachersData; }
+            set
+            {
+                teachersData = value;
+                OnPropertyChanged("TeachersData");
+            }
+        }
+
+        public void FetchRating()
+        {
+            List<RatingTeacherData> data = [];
+            List<Teachers> teachers = App.db.Teachers.ToList();
+
+            foreach (Teachers teacher in teachers)
+            {
+                List<Raports> raports = App.db.Raports.Where(p => p.teacher_id == teacher.id && p.date.Year == DateTime.Today.Year).ToList();
+             
+                double hours = 0;
+
+                foreach(Raports raport in raports)
+                {
+                    hours += raport.hours;
+                }
+
+                hours = Math.Round(hours, 2);
+
+                data.Add(new RatingTeacherData(0, teacher.name + " " + teacher.surname, hours, teacher, raports));
+            }
+
+            data.Sort((x, y) => y.hours.CompareTo(x.hours));
+
+            int i = 1;
+            foreach (RatingTeacherData teacherData in data)
+            {
+                teacherData.number = i;
+                i++;
+            }
+
+            TeachersData = data;
+        }
     }
 }
--- Project workshop/UniversityServer/Views/RatingView.xaml.cs
+++ Project workshop/UniversityServer/Views/RatingView.xaml.cs
@@ -1,28 +1,89 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Win32;
+using ScottPlot;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using UniversityServer.Database;
+using UniversityServer.ViewModels;
 
 namespace UniversityServer.Views
 {
-    /// <summary>
-    /// Interaction logic for RatingView.xaml
-    /// </summary>
     public partial class RatingView : UserControl
     {
+        private readonly RatingViewModel viewModel = new();
+
         public RatingView()
         {
             InitializeComponent();
+            DataContext = viewModel;
+            viewModel.FetchRating();
+            WpfPlot1.Plot.XLabel("Mouth");
+            WpfPlot1.Plot.YLabel("Hours");
+            WpfPlot1.Plot.Title("Hours per Mouth");
+        }
+
+        private void RatingViewList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            try
+            {
+                PrintButton.IsEnabled = true;
+
+                DateTime[] dates = GetMonthsOfYear();
+
+                double[] dataY = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+
+                foreach(Raports raport in ((RatingTeacherData)RatingViewList.SelectedItem).raports)
+                {
+                    dataY[raport.date.Month - 1] += raport.hours;
+                }
+
+                WpfPlot1.Plot.Clear();
+                WpfPlot1.Plot.Axes.DateTimeTicksBottom();
+                WpfPlot1.Plot.Axes.SetLimits(dates[0].ToOADate(), dates[11].ToOADate(), 0, 100);
+                WpfPlot1.Plot.Add.Scatter(dates, dataY);
+                WpfPlot1.Refresh();
+
+                WpfPlot1.Visibility = Visibility.Visible;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private DateTime[] GetMonthsOfYear()
+        {
+            DateTime[] months = new DateTime[12];
+            DateTime currentDate = DateTime.Today;
+
+            for (int i = 0; i < 12; i++)
+            {
+                months[i] = new DateTime(currentDate.Year, i + 1, 1);
+            }
+
+            return months;
+        }
+
+        private void PrintButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                SaveFileDialog saveFileDialog = new SaveFileDialog();
+
+                saveFileDialog.Filter = "PNG files (*.png)|*.png|All files (*.*)|*.*";
+
+                if (saveFileDialog.ShowDialog() == true)
+                {
+                    string filePath = saveFileDialog.FileName;
+
+                    WpfPlot1.Plot.SavePng(filePath, 400, 300);
+
+                    MessageBox.Show("График успешно сохранен.", "Успех", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
